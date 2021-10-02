17660175054fb322f35a61c1fd63bb1159cd07b8
johannpereze <sebas.jsp@gmail.com>
2021-10-02 18:38:27 -0500

    Hago la primera peticion http por nombre de pais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppComponent } from './app.component';
 import { PaisModule } from './pais/pais.module';
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, PaisModule, SharedModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, PaisModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
