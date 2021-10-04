ddaadd01f1a97294acd952229453434e49b7b4fe
johannpereze <sebas.jsp@gmail.com>
2021-10-04 15:05:11 -0500

    Creo la vista de pais con alguna información y sus traducciones

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PaisService } from '../../services/pais.service';
+import { switchMap, tap } from 'rxjs/operators';
+import { Country } from '../../interfaces/pais.interfaces';
 
 @Component({
   selector: 'app-ver-pais',
@@ -8,16 +10,19 @@ import { PaisService } from '../../services/pais.service';
   styles: [],
 })
 export class VerPaisComponent implements OnInit {
+  pais!: Country;
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private paisService: PaisService
   ) {}
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe((params) => {
-      this.paisService.getPaisPorAlpha(params.id).subscribe((pais) => {
-        console.log(pais);
-      });
-    });
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({ id }) => this.paisService.getPaisPorAlpha(id)),
+        tap(console.log)
+      ) //con switchMap nos ahorramos tener 2 subscribe
+      .subscribe((pais) => this.pais = pais[0]);
   }
 }
