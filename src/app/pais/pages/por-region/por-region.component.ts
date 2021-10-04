70a296e8b8e3274bd6f92d4a2fb91cfa9196338b
johannpereze <sebas.jsp@gmail.com>
2021-10-04 16:16:26 -0500

    Organizo los botones de las regiones en el componente por-region

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,16 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
   styles: [
-  ]
+    `
+      button {
+        margin-right: 5px;
+      }
+    `,
+  ],
 })
-export class PorRegionComponent implements OnInit {
+export class PorRegionComponent {
+  regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  regionActiva: string = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  getCSSClass(region: string): string {
+    return region === this.regionActiva
+      ? 'btn btn-primary'
+      : 'btn btn-outline-primary';
   }
 
+  constructor() {}
+
+  activarRegion(region: string) {
+    this.regionActiva = region;
+  }
 }
